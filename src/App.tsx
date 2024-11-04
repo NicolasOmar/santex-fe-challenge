aa4af87590e8e8a9a902ed2dbf1a99868342a97b
NicolasOmar <nicolas.passerino@gmail.com>
2024-11-04 15:47:31 -0300

    Moving project from create-react-app to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Header } from './components/Header';
-import { ProductList } from './components/ProductList';
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Header } from './components/Header'
+import { ProductList } from './components/ProductList'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <Header></Header>
-      <div>
+      <section>
         <ProductList></ProductList>
-      </div>
+      </section>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export function ProductList() {
   return <div>List</div>;
 }
