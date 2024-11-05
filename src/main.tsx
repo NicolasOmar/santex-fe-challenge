1cf6636dd6445cd3e8f25680924837b92a51965f
NicolasOmar <nicolas.passerino@gmail.com>
2024-11-05 18:03:18 -0300

    Adding mising apollo graphql configs (from former test configuration)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client';
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+
+const API_URL = 'https://demo.vendure.io/shop-api/shop-api'
+
+const commerceLink = createHttpLink({
+  uri: API_URL,
+  headers: {
+    authorization: localStorage.getItem('Auth-Token')
+      ? `Bearer ${localStorage.getItem('Auth-Token')}`
+      : '',
+  },
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    const context = operation.getContext();
+    const authHeader = context.response.headers.get('Vendure-Auth-Token');
+
+    if (authHeader) {
+      localStorage.setItem('Auth-Token', authHeader);
+    }
+    
+    return response;
+  });
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([afterwareLink, commerceLink]),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
